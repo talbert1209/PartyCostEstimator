de71eb644b84d5d2aefef5a65cc412a4443a57fc
talbert1209 <talbert1209@gmail.com>
2018-09-20 14:36:23 -0500

    Completed DinnerParty class

--- PartyCostEstimator/DinnerParty.cs
+++ PartyCostEstimator/DinnerParty.cs
@@ -19,18 +19,29 @@
             }
         }
 
-        public void CalculateCostOfDecorations()
+        public void CalculateCostOfDecorations(bool fancy)
         {
-
+            if (fancy)
+            {
+                CostOfDecorations = (NumberOfPeople * 15.00M) + 50.00M;
+            }
+            else
+            {
+                CostOfDecorations = (NumberOfPeople * 7.50M) + 30.00M;
+            }
         }
 
         public decimal CalculateCost()
         {
+            var totalCost = (CostOfFoodPerPerson * NumberOfPeople) + (CostOfBeveragesPerPerson * NumberOfPeople) + CostOfDecorations;
 
-            if (SetHealthyOption())
+            if (CostOfBeveragesPerPerson == 5.00M)
             {
-                
+                var discount = .05M * totalCost;
+                return totalCost - discount;
             }
+
+            return totalCost;
         }
 
     }
