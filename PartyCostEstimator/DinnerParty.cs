d7a1939780996a532196acc15a6666d80e78ccf7
talbert1209 <talbert1209@gmail.com>
2018-09-20 13:58:54 -0500

    Add project files.

---
+++ PartyCostEstimator/DinnerParty.cs
@@ -0,0 +1,37 @@
+﻿namespace PartyCostEstimator
+{
+    public class DinnerParty
+    {
+        public int NumberOfPeople;
+        public decimal CostOfBeveragesPerPerson;
+        public decimal CostOfDecorations;
+        public const decimal CostOfFoodPerPerson = 25.00M;
+
+        public void SetHealthyOption(bool healthyOption)
+        {
+            if (healthyOption)
+            {
+                CostOfBeveragesPerPerson = 5.00M;
+            }
+            else
+            {
+                CostOfBeveragesPerPerson = 20.00M;
+            }
+        }
+
+        public void CalculateCostOfDecorations()
+        {
+
+        }
+
+        public decimal CalculateCost()
+        {
+
+            if (SetHealthyOption())
+            {
+                
+            }
+        }
+
+    }
+}
